f164e7d99b16828ac7dba9578a81ebbb4c2bb97a
Asher Spector <amspector100@gmail.com>
2018-09-15 13:39:14 -0400

    Updated mapping, got rid of pointwise restaurant data

--- mapping.py
+++ mapping.py
@@ -12,13 +12,29 @@ def plot_nta_boundaries(save_path = 'plots/interactive.html'):
 
 	# Read in data and simplify (very complex) boundaries a little bit to reduce file size
 	boundary_data = gpd.read_file(nta_boundaries_path)
+	boundary_data.set_index('ntacode', inplace = True)
 	boundary_data['geometry'] = boundary_data['geometry'].simplify(tolerance = 0.0005)
 
-	basemap = folium.Map([nyc_lat, nyc_long],
-                     zoom_start=11)	
+	# Add sample boroughs layer
+	basemap = folium.Map([nyc_lat, nyc_long],zoom_start=11)	
 
 	choropleth.categorical_choropleth(boundary_data, factor = 'boroname', name = 'Boroughs',
 									quietly = True, basemap = basemap)
+
+	# Get request data
+	request_data = pd.read_csv('data/cleaned/cleaned_311.csv')
+	request_data = request_data.loc[(request_data['ntacode'].notnull())]
+
+	# Add layer for closure rates
+	request_data['closed_flag'] = request_data['status'].apply(lambda x: x == 'Closed')
+	boundary_data['closure_rate'] = 100*request_data.groupby(['ntacode'])['closed_flag'].mean()
+
+	# Add layer for closure dates
+
+
+	choropleth.continuous_choropleth(boundary_data, factor = 'closure_rate', layer_name = '311 Service Request Closure Rate', quants = None, 
+									 basemap = basemap)
+
 
 	folium.TileLayer('cartodbdark_matter').add_to(basemap)
 	folium.LayerControl().add_to(basemap)
