9553c0bedeeef74bd13fe16ec95302c9bfc925b3
Asher Spector <amspector100@gmail.com>
2018-09-15 12:36:02 -0400

    Initial commit for gh-pages

--- restaurants_inspections.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def get_inspection_count(fpath):
-	data = pd.read_csv(fpath, engine='python')
-	cleaned_data = data.drop_duplicates(['facility', 'address', 'inspection_date'])
-	return cleaned_data
--- utilities/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Sub-package containing (almost) all of the utility functions for the project"""
-__all__ = ['simple', 'spatial_joins', 'measurements']
-
-from . import simple
-from . import spatial_joins
-from . import measurements
